9b5b53ca242de1400b4912087e62569327cb792e
Bartomiej Zapart <bartlomiej.zapart@argosmultilingual.com>
2021-05-10 21:12:59 +0200

    !!!!

---
+++ Toci.Rescue/Toci.Rescue.Integrator.Interfaces/IIntegratorEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Rescue.Integrator.Interfaces
+{
+    public interface IIntegratorEntity
+    {
+        Dictionary<string, ICompany> Companies { get; set; }
+
+        Dictionary<string, IUnemployed> Unemployed { get; set; }
+
+        Dictionary<string, IEstate> Estates { get; set; }
+    }
+}
